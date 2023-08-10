40a9035d3c7a5407dfee5cbea227a0b63add079f
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-10 14:14:02 +0100

    128. Longest Consecutive Sequence

--- arrays_and_hashing/meduim/128_Longest_Consecutive_Sequence/Solution.py
+++ arrays_and_hashing/meduim/128_Longest_Consecutive_Sequence/Solution.py
@@ -1,6 +1,13 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        return -1
-    
+        hashset, maximum = set(nums), 0
+        for i in hashset:
+            if i-1 not in hashset:
+                sequence = 1
+                while (i+sequence) in hashset:
+                    sequence += 1
+                maximum = max(maximum, sequence)
+        return maximum
 
-print(Solution().longestConsecutive([100,4,200,1,3,2]))
+
+print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
