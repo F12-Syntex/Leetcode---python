5a67c11c959ea9e792a8992e01c010f398e04065
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-13 12:15:13 +0100

    42. Trapping Rain Water

--- two_pointers/hard/42_Trapping_Rain_Water/Solution.py
+++ two_pointers/hard/42_Trapping_Rain_Water/Solution.py
@@ -1,6 +1,28 @@
 class Solution(object):
     def trap(self, height):
-        return -1
+
+        length = len(height)
+
+        l, r, level = 0, length - 1, 0
+        maxL, maxR = height[l], height[r]
+
+        while l < r:
+
+            waterLevel = 0
+
+            if maxL < maxR:
+                l += 1
+                waterLevel = maxL - height[l]
+                maxL = maxL if maxL > height[l] else height[l]
+            else:
+                r -= 1
+                waterLevel = maxR - height[r]
+                maxR = maxR if maxR > height[r] else height[r]
+
+            if waterLevel > 0:
+                level += waterLevel
+
+        return level
 
 arr = [0,1,0,2,1,0,1,3,2,1,2,1]
 print(Solution().trap(arr))
