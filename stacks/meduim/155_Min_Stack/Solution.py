bf5f2e6f47b6159ec2c42b81348f9c9c7283c1da
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-14 15:09:00 +0100

    155. Min Stack

---
+++ stacks/meduim/155_Min_Stack/Solution.py
@@ -0,0 +1,38 @@
+class MinStack(object):
+
+    def __init__(self):
+        pass
+        
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
