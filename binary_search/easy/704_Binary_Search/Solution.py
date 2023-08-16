ed19ca64d3815e45e8e72d89db924545f489d423
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-16 21:38:53 +0100

    74_Search_A_2D_Matrix

--- binary_search/easy/704_Binary_Search/Solution.py
+++ binary_search/easy/704_Binary_Search/Solution.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def search(self, nums, target):
-
-        l, r = 0, len(nums)
-
-        while(l < r):
+        l, r = 0, len(nums) - 1
+        while l <= r:
             middleIndex = l + ((r - l) // 2)
-            if nums[middleIndex] < target:
-                if l == middleIndex:
-                    return -1
-                l = middleIndex
-            elif nums[middleIndex] > target:
-                if r == middleIndex:
-                    return -1
-                r = middleIndex
-            else:
+            if nums[middleIndex] == target:
                 return middleIndex
-
-
+            if nums[middleIndex] < target:
+                l = middleIndex + 1
+            else:
+                r = middleIndex - 1
         return -1
     
 
---
+++ binary_search/meduim/74_Search_a_2D_Matrix/Solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+
+        m = len(matrix)
+        n = len(matrix[0])
+        length = m * n
+
+        l, r = 0, length - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            arrayIndex = m // n
+            cellIndex = m - (arrayIndex * n)
+
+            print(m)
+
+            if matrix[arrayIndex][cellIndex] == target:
+                return True
+            elif matrix[arrayIndex][cellIndex] < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return False
+    
+
+print(Solution().searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
