b1b6545019673ae3a5ebd2d57b2ce780420043d8
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-25 22:42:12 +0100

    424. Longest Repeating Character Replacement

--- sliding_window/meduim/424_Longest_Repeating_Character_Replacement/Solution.py
+++ sliding_window/meduim/424_Longest_Repeating_Character_Replacement/Solution.py
@@ -1,5 +1,19 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        return -1
+
+        l = 0
+        freq = {}
+        curr = 0
+
+        for r in range(len(s)):
+            freq[s[r]] = freq.get(s[r], 0) + 1
+            
+            while (r - l + 1) - max(freq.values()) > k:
+                freq[s[l]] -= 1
+                l+=1
+
+            curr = max(curr, (r - l) + 1)
+
+        return curr
 
 print(Solution().characterReplacement("ABAB", 2))
