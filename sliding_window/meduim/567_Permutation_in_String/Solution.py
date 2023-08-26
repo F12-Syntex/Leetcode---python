8d6c2b692dd46ced6c6b5e588e5d4d6e7c5f475c
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-26 21:23:50 +0100

    567. Permutation in String

--- sliding_window/meduim/567_Permutation_in_String/Solution.py
+++ sliding_window/meduim/567_Permutation_in_String/Solution.py
@@ -1,8 +1,28 @@
+import string
 class Solution(object):
-    def checkInclusion(self, s1, s2):
+   def checkInclusion(self, s1, s2):
+        
+        freqS1 = {i: 0 for i in string.ascii_lowercase}
+        freqS2 = {i: 0 for i in string.ascii_lowercase}
 
-        print(s1)
+        for i in s1:
+            freqS1[i] += 1
+
+        l = 0
+        for r in range(len(s2)):
+            freqS2[s2[r]] += 1
+
+            if (r - l + 1) > len(s1):
+                freqS2[s2[l]] -= 1
+                l+=1
+
+
+            matched = True
+            for i in string.ascii_letters:
+                if freqS1[i] != freqS2[i]:
+                    matched = False
+                    break
 
         return False
     
-print(Solution().checkInclusion("ab", "eidbaooo"))
+print(Solution().checkInclusion("ba", "eidbaoo"))
