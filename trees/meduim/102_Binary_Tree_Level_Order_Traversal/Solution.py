6e2c31254bf6957954e66d4668af6df7931aedf6
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-09-13 16:16:53 +0100

    102. Binary Tree Level Order Traversal

---
+++ trees/meduim/102_Binary_Tree_Level_Order_Traversal/Solution.py
@@ -0,0 +1,57 @@
+import collections
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def __repr__(self):
+        return str(self.val)
+    
+    def __str__(self):
+        output = []
+        self.pretty_print(output, 0)
+        return '\n'.join(output)
+    
+    def pretty_print(self, output, level=0):
+        prefix = '    ' * level
+        output.append(f'{prefix}{self.val}')
+        if self.left:
+            self.left.pretty_print(output, level + 1)
+        if self.right:
+            self.right.pretty_print(output, level + 1)
+
+    @staticmethod
+    def build_tree(arr, index=0):
+        if index >= len(arr) or arr[index] is None:
+            return None
+
+        root = TreeNode(arr[index])
+        root.left = TreeNode.build_tree(arr, 2 * index + 1)
+        root.right = TreeNode.build_tree(arr, 2 * index + 2)
+        return root
+    
+
+class Solution(object):
+    def levelOrder(self, root):
+        q = collections.deque()
+        res = []
+        q.append(root)
+        while q:
+            length = len(q)
+            level = []
+            for i in range(length):
+                cur = q.popleft()
+                if cur:
+                    level.append(cur.val)
+                    q.append(cur.left)
+                    q.append(cur.right)
+            if len(level) > 0:
+                res.append(level)
+        return res
+
+root = TreeNode.build_tree([1, 2, 3, 4, 5])
+sameTree = Solution().levelOrder(root)
+print(sameTree)
+
